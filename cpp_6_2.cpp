cd992941d481f9f6d73b5b0408ccb6c32de72d44
solda <solda@khu.ac.kr>
2021-03-26 21:04:57 +0900

    updated

--- cpp_6_2.cpp
+++ cpp_6_2.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void doSomething(int students_scores[20])     // 엄밀히 따지면 배열이 아님
+// 배열과 포인터는 뗄래야 뗄 수 없는 사이
+
+void doSomething(int students_scores[])       // 엄밀히 따지면 배열이 아님
 {                                             // 문법 상 포인터라고 봄
     cout << (int)&students_scores << endl;    // 주소값을 저장하는 다른 변수의 주소 출력
     cout << (int)&students_scores[0] << endl; // 포인터 변수에 저장되어 있는 값의 주소
     cout << students_scores[0] << endl;
     cout << students_scores[1] << endl;
     cout << students_scores[2] << endl;
-    cout << "Size in doSomthing " << sizeof(students_scores) << endl;
+    cout << "Size in doSomthing " << sizeof(students_scores) << endl; // x64에서는 포인터 변수가 8바이트, 32비트에서는 4바이트
 }
 
 int main()
@@ -27,7 +29,7 @@ int main()
     cout << students_scores[0] << endl;
     cout << students_scores[1] << endl;
     cout << students_scores[2] << endl;
-    cout << "Size in doSomthing " << sizeof(students_scores) << endl;
+    cout << "Size in main " << sizeof(students_scores) << endl;
 
     doSomething(students_scores); // 배열의 모든 원소를 복사하는 것이 아니라 배열의 첫번째 주소 값만 복사
 }
---
+++ cpp_6_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int num_students = 5;
+    int scores[num_students] = { 84, 92, 76, 81, 56 };
+
+    int total_score = 0;
+    for (int i = 0; i < num_students; i++)
+    {
+        total_score += scores[i];
+    }
+
+    double avg_score = static_cast<double>(total_score) / num_students; // 나누기 전에 casting 한 것과 나누기 한 후에 casting 한 것은 다름
+
+    cout << "HELLO" << endl;
+    cout << avg_score << endl;
+}
